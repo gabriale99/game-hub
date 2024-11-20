394b14c37fb581e3a80ffec1d8c2ea39109aaea9
gabriale99 <gabriale99@gmail.com>
2024-11-19 18:01:22 -0800

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} gap={2} mx={2}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} gap={3} mx={2}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
 import useGenres from "@/hooks/useGenre";
+import getCroppedImageURL from "@/services/image-url";
+import { HStack, Image, List, Text } from "@chakra-ui/react";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
-      {data.map((gen) => (
-        <li key={gen.name}>{gen.name}</li>
+    <List.Root variant="plain">
+      {data.map((genre) => (
+        <List.Item key={genre.name} py="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageURL(genre.image_background)}
+            ></Image>
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </List.Item>
       ))}
-    </ul>
+    </List.Root>
   );
 };
 
