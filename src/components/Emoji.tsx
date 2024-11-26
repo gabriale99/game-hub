8b0ca3a66d8c972b4937f2a524650d75147e40c4
gabriale99 <gabriale99@gmail.com>
2024-11-26 12:56:24 -0800

    Set emoji to have same sizes

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -11,8 +11,8 @@ const Emoji = ({ rating }: Props) => {
   if (rating < 3) return null;
 
   const emojiMap: { [key: number]: ImageProps } = {
-    3: { src: meh, alt: "alt", boxSize: "25px" },
-    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    3: { src: meh, alt: "alt", boxSize: "35px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "35px" },
     5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
   };
   return <Image {...emojiMap[rating]} mt={1} />;
